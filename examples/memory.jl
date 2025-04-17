9a403574c751c101fc195fc43d29ce26a08efe4d
Vasil Topalovic <vasiltopalovic@outlook.com>
2025-04-16 20:30:37 -0400

    docs: add memory example

---
+++ examples/memory.jl
@@ -0,0 +1,15 @@
+module "main"
+import "std:io"
+
+Test :: struct {
+	a: i32
+}
+
+main :: () i32 {
+	let test: Test = Test { a: 5 };	
+	let other: ^Test = &test;
+
+	let new: Test = ^other;
+	io:print("%d\n", new.a);
+	return 0;
+}
